e306dc6f5692c1f8a777a502469f5378fd2b73b1
keivanipchihagh <ipchi1380@gmail.com>
2021-06-22 10:41:49 +0430

    Ten Most Active Characters

--- .virtual_documents/2.Exploratory data analysis.ipynb.py
+++ .virtual_documents/2.Exploratory data analysis.ipynb.py
@@ -1,92 +1,29 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
-import re
-import string
 import spacy
-from wordcloud import WordCloud
-from spacytextblob.spacytextblob import SpacyTextBlob
 
-nlp = spacy.load('en_core_web_sm')
-nlp.add_pipe('spacytextblob')
+nlp = spacy.load('en_core_web_md')
 
 
-scripts_df = pd.read_csv('Data/Processed/simpsons_cleaned_script_lines.csv')
-episods_df = pd.read_csv('Data/Processed/simpsons_cleaned_episodes.csv')
-
-scripts_df.head()
+scripts_df = pd.read_csv('Data/Processed/scripts.csv')
+episods_df = pd.read_csv('Data/Processed/episodes.csv')
 
 
-all_words = []
+top_ten_active_characters = scripts_df.groupby(
+    by = 'character',
+    as_index = False
+).count().sort_values(
+    by = 'raw_text',
+    ascending = False
+).iloc[:10]
 
-for i, raw_text in enumerate(scripts_df['raw_text']):
-    
-    # Convert to lowercase
-    raw_text = raw_text.lower()
-    
-    # Lemmatize and remove stop words
-    new_text = ' '.join([token.lemma_ for token in nlp(raw_text) if token.lemma_ not in nlp.Defaults.stop_words])
-    
-    # Remove punctuation
-    new_text = new_text.translate(str.maketrans('', '', string.punctuation))
-    
-    # Split and add to all_wrds list
-    all_words += new_text.split()
-    
-    # Log
-    if i % 1000 == 0:
-        print(f'Processing {i} Script...')
+plot = top_ten_active_characters[['character', 'raw_text']].plot.bar(x = 'character', y = 'raw_text', label = 'Dialogs', rot = 45)
+fig = plot.get_figure()
+fig.savefig("Plots/top_ten_active_characters.png")
 
 
-word_freq = pd.Series(all_words).value_counts().sort_values(ascending = False)
 
-plt.barh(word_freq[:20].keys()[::-1], word_freq[:20].values[::-1])
-plt.title('Twenty Most Common Words')
-plt.xlabel('Frequency')
-plt.ylabel('Words')
-plt.show()
-
-
-characters_freq = scripts_df['character'].value_counts().sort_values(ascending = False)
-
-plt.barh(characters_freq[:10].keys()[::-1], characters_freq[:10].values[::-1])
-plt.title('Ten Most Active Words')
-plt.xlabel('Frequency')
-plt.ylabel('Character')
-plt.show()
-
-
-wc = WordCloud(
-    background_color = "white",
-    colormap = "Dark2",
-    max_font_size = 150,
-)
-
-wc.generate(' '.join(all_words))
-
-plt.imshow(wc, interpolation = 'spline36')
-plt.axis("off")
-plt.show()
-
-
-polarities = []
-subjectivities = []
-
-for i, raw_text in enumerate(scripts_df['raw_text']):
-    
-    doc = nlp(raw_text)
-    polarities.append(doc._.polarity)
-    subjectivities.append(doc._.subjectivity)
-    
-    if i % 1000 == 0:
-        print(f'Processing {i} Script...')
-
-
-scripts_df['polarity'] = polarities
-scripts_df['subjectivity'] = subjectivities
-
-# Save new DataFrame
-scripts_df.to_csv('Data/Processed/sentimented_script_lines.csv', index = False)
 
 
 
