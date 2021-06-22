1dad2cf1a79c021e4328039d938192a9c56ad9c4
keivanipchihagh <ipchi1380@gmail.com>
2021-06-22 11:40:21 +0430

    Word Clouds

--- .virtual_documents/2.Exploratory data analysis.ipynb.py
+++ .virtual_documents/2.Exploratory data analysis.ipynb.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from wordcloud import WordCloud
 import spacy
 
 nlp = spacy.load('en_core_web_md')
@@ -22,22 +23,35 @@ top_ten_active_characters = scripts_df.groupby(
 
 plot = top_ten_active_characters[['character', 'raw_text']].plot.bar(x = 'character', y = 'raw_text', label = 'Dialogs', rot = 30)
 fig = plot.get_figure()
+plt.title("Ten Most Active Characters")
 fig.set_size_inches(20, 5)
 fig.savefig("Plots/top_ten_active_characters.png")
 plt.show()
 
 
 normalized_text_values = scripts_df.loc[scripts_df['normalized_text'].notnull(), 'normalized_text'].values
-twenty_top_common_words = pd.Index(' '.join(normalized_text_values).split(' ')).value_counts()[:20]
+all_normalized_text = ' '.join(normalized_text_values)
+twenty_top_common_words = pd.Index(all_normalized_text.split(' ')).value_counts()[:20]
 
-fig = plt.figure(figsize = (20, 5))
-plt.bar(x = twenty_top_common_words.index, height = twenty_top_common_words.values, label = 'Repetition')
-plt.legend()
+fig, axs = plt.subplots(1, 2, figsize = (20, 5))
+
+# Bar Plot
+axs[0].bar(x = twenty_top_common_words.index, height = twenty_top_common_words.values, label = 'Repetition')
+axs[0].legend()
+axs[0].tick_params(labelrotation = 45)
+
+# Word Cloud
+wordcloud = WordCloud(
+    background_color = 'white',
+    max_words = 70,
+).generate(all_normalized_text)
+
+axs[1].set_title("WordCloud of Twenty Top Common Words")
+axs[1].imshow(wordcloud, interpolation = 'bilinear')
+axs[1].axis("off")
+
 plt.savefig('Plots/twenty_top_common_words.png')
 plt.show()
-
-
-
 
 
 
